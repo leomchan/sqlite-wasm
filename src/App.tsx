ada02ecc478744b788f144b4d9977ec2ac2fdac5
Leo Chan <leo.chan@tokuindustry.com>
2023-11-18 16:06:58 -0700

    add xterm

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from 'react'
+import 'xterm/css/xterm.css';
+import { Terminal } from 'xterm';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const terminalRef = useRef<HTMLDivElement>(null);
+  const [terminal] = useState(new Terminal());
+
+  useEffect(() => {
+    if (terminalRef.current) {
+      terminal.open(terminalRef.current);
+      terminal.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
+    }
+  }, [terminal]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div ref={terminalRef}></div>
     </>
   )
 }
